e0fd7cb22ac54eadd1479d1b33f599c3bb5374dd
xceso <notxceso@gmail.com>
2025-09-24 19:20:35 +0300

    refactor(domain): DeliveryPoint no longer tracks Parcels

--- Manta.Presentation/Program.cs
+++ Manta.Presentation/Program.cs
@@ -13,6 +13,7 @@ public static class Manta
         EventsLoader.LoadAllEvents(statusService);
         ParcelDeliveryService deliveryService = new ParcelDeliveryService(statusService);
         var dp = DeliveryPointFactory.Create(1, "Kyiv");
+        var dp2 = DeliveryPointFactory.Create(2, "Lviv");
         var parcel = ParcelFactory.Create(new ParcelCreationOptions(
             Id: 1,
             DeliveryPointId: 1,
@@ -22,8 +23,20 @@ public static class Manta
             RecipientEmail: "popa@gmail.com",
             Weight: 10,
             CreatedBy: SystemUser.Instance));
+        var parcel2 = ParcelFactory.Create(new ParcelCreationOptions(
+            Id: 2,
+            DeliveryPointId: 2,
+            AmountDue: 1000,
+            RecipientName: "John",
+            RecipientPhoneNumber: "+380987654321",
+            RecipientEmail: "popa@gmail.com",
+            Weight: 10,
+            CreatedBy: SystemUser.Instance));
         deliveryService.CancelParcel(parcel, SystemUser.Instance);
         deliveryService.AcceptedAtDeliveryPoint(dp, parcel, SystemUser.Instance);
+        deliveryService.AcceptedAtDeliveryPoint(dp2, parcel2, SystemUser.Instance);
+        deliveryService.ReaddressParcel(dp, parcel2, SystemUser.Instance);
+        deliveryService.DeliverParcel(dp2, parcel2, SystemUser.Instance);
         // var parcel1 = ParcelFactory.Create(1, 1, SystemUser.Instance);
         // var parcel2 = ParcelFactory.Create(2, 2, SystemUser.Instance);
         // deliveryService.AcceptedAtDeliveryPoint(dp,parcel1,SystemUser.Instance);
